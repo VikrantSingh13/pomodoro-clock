f9ed46db66dfb125dce3a3fdb9aace6f5b9f3e71
vikrantsingh13 <vikrantsinghvm@gmail.com>
2017-04-17 19:48:50 -0400

    added a display for better UX

--- js/index.js
+++ js/index.js
@@ -9,16 +9,25 @@ var mins = parseInt(document.getElementById('session-time').innerText),
 //add sound files when going on break and ending break
 function minus(){
   if(secs == 0 && mins == 0){
+    
     if(!onBreak){
       mins = parseInt(document.getElementById('break-time').innerText),
         onBreak = true,
         onPause = true;
+      
+      document.getElementById('on-break').classList.add('visible');
+      
       clearTimeout(t);
       startTimer();
+      
     } else {
+      
       mins = parseInt(document.getElementById('session-time').innerText),
         onBreak = false,
         onPause = true;
+      
+      document.getElementById('on-break').classList.remove('visible');
+      
       clearTimeout(t);
       startTimer();      
     }
@@ -37,7 +46,7 @@ function minus(){
 }
 
 function timer(){
-  t = setTimeout(minus, 500);
+  t = setTimeout(minus, 1000);
 }
 
 function startTimer(){
@@ -53,15 +62,23 @@ function startTimer(){
   }
   else if(onPause){
     onPause = false;
+    
+    document.getElementById('paused').classList.remove('visible');
+    
     timer();
   } else {
     onPause = true;
+    document.getElementById('paused').classList.add('visible');
     clearTimeout(t);
   }  
 }
 
 function timerDec(str){
+  
+  document.getElementById('paused').classList.add('visible');
+  
   clearTimeout(t);
+  
   var tempTimer = document.getElementById(str + "-time").innerText;
   document.getElementById(str + "-time").innerText = parseInt(tempTimer) - 1;
   
@@ -69,7 +86,11 @@ function timerDec(str){
 }
 
 function timerInc(str){
+  
+  document.getElementById('paused').classList.add('visible');
+  
   clearTimeout(t);
+  
   var tempTimer = document.getElementById(str + "-time").innerText;
   document.getElementById(str + "-time").innerText = parseInt(tempTimer) + 1;
   
